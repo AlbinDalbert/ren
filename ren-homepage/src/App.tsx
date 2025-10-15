1d381710e5a738ed652ddfb6570e2c4c3e6f911a
Albin Dalbert <albin.dalbert@protonmail.com>
2025-10-15 22:33:18 +0200

    basic init of the app

---
+++ ren-homepage/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="container">
+      <div className="card albin-dalbert">
+        <h1>Albin Dalbert</h1>
+      </div>
+      <div className="card ninth-sentinel">
+        <h1>Ninth Sentinel</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
